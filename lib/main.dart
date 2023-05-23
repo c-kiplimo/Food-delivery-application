7b15ad58a70a6f1304e3e3678b8723427ff12ae5
c-kiplimo <kiplimocollins855@gmail.com>
2023-05-23 03:42:21 +0300

    feat:login signup page,profilePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,370 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:food_delivery/bloc/listTileColorBloc.dart';
+import './loginpage/sigin_page.dart';
+import './const/themeColor.dart';
+import 'package:bloc_pattern/bloc_pattern.dart';
+import './loginpage/signup_page.dart';
+import './bloc/listTileColorBloc.dart';
+import 'firstpage.dart';
+
 
 void main() {
-  runApp(const MyApp());
+  debugDefaultTargetPlatformOverride =TargetPlatform.fuchsia;
+   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return BlocProvider(
+    blocs: [
+        //add yours BLoCs controlles
+        Bloc((i) => ColorBloc()),
+      ],
+      child: MaterialApp(
+        title: "Food Tukk",
+        home: FirstPage(),
+        debugShowCheckedModeBanner: false,
+        routes: <String, WidgetBuilder>{
+          '/landingpage': (BuildContext context) => new MyApp(),
+          '/signup': (BuildContext context) => new SignUpPage(),
+          '/homepage': (BuildContext context) => new Home(),
+          '/signin': (BuildContext context) => new SignInPage(),
+          '/firstpage': (BuildContext context) => new FirstPage(),
+        },
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Home extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomeState createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+resizeToAvoidBottomInset: false,
+      drawer: Drawer(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.all(20),
+              color: Themes.color,
+              child: Center(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      width: 100,
+                      height: 100,
+                      margin: EdgeInsets.only(top: 30, bottom: 10),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                'https://cdn2.iconfinder.com/data/icons/website-icons/512/User_Avatar-512.png'),
+                            fit: BoxFit.fill),
+                      ),
+                    ),
+                    Text(
+                      'Name: user',
+                      style: TextStyle(fontSize: 22, color: Colors.black87),
+                    ),
+                    Text(
+                      'Email: user@gmail.com',
+                      style: TextStyle(color: Colors.black87),
+                    )
+                  ],
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text(
+                'Setting',
+                style: TextStyle(fontSize: 18),
+              ),
+              onTap: null,
+            ),
+            ListTile(
+              leading: Icon(Icons.arrow_back),
+              title: Text(
+                'Logout',
+                style: TextStyle(fontSize: 18),
+              ),
+              onTap: () {
+               // To be implemented
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: SafeArea(
+          child: Container(
+        child: ListView(
+          children: <Widget>[
+            FirstHalf(),
+            SizedBox(height: 45),
+           // impelement
+          ],
+        ),
+      )),
     );
   }
 }
+
+
+class FirstHalf extends StatelessWidget {
+  const FirstHalf({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(35, 25, 0, 0),
+      child: Column(
+        children: <Widget>[
+          
+        ],
+      ),
+    );
+  }
+}
+
+
+class Items extends StatelessWidget {
+  Items({
+    @required this.leftAligned,
+    @required this.imgUrl,
+    @required this.itemName,
+    @required this.itemPrice,
+    @required this.hotel,
+  });
+
+  final bool leftAligned;
+  final String imgUrl;
+  final String itemName;
+  final double itemPrice;
+  final String hotel;
+
+  @override
+  Widget build(BuildContext context) {
+    double containerPadding = 45;
+    double containerBorderRadius = 10;
+
+    return Column(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.only(
+            left: leftAligned ? 0 : containerPadding,
+            right: leftAligned ? containerPadding : 0,
+          ),
+          child: Column(
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                height: 200,
+                decoration:
+                    BoxDecoration(borderRadius: BorderRadius.circular(10)),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.horizontal(
+                    left: leftAligned
+                        ? Radius.circular(0)
+                        : Radius.circular(containerBorderRadius),
+                    right: leftAligned
+                        ? Radius.circular(containerBorderRadius)
+                        : Radius.circular(0),
+                  ),
+                  child: Image.network(
+                    imgUrl,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                  padding: EdgeInsets.only(
+                    left: leftAligned ? 20 : 0,
+                    right: leftAligned ? 0 : 20,
+                  ),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: Text(itemName,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w700,
+                                    fontSize: 18,
+                                  )),
+                            ),
+                            Text("\₹$itemPrice",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 18,
+                                )),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: RichText(
+                            text: TextSpan(
+                                style: TextStyle(
+                                    color: Colors.black45, fontSize: 15),
+                                children: [
+                                  TextSpan(text: "by "),
+                                  TextSpan(
+                                      text: hotel,
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w700))
+                                ]),
+                          ),
+                        ),
+                        SizedBox(height: containerPadding),
+                      ])),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class CategoryListItem extends StatelessWidget {
+  const CategoryListItem({
+    Key key,
+    @required this.categoryIcon,
+    @required this.categoryName,
+    @required this.availability,
+    @required this.selected,
+  }) : super(key: key);
+
+  final IconData categoryIcon;
+  final String categoryName;
+  final int availability;
+  final bool selected;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(right: 20),
+      padding: EdgeInsets.fromLTRB(10, 10, 10, 20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(40),
+        color: selected ? Themes.color : Colors.white,
+        border: Border.all(
+            color: selected ? Colors.transparent : Colors.grey[200],
+            width: 1.5),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey[100],
+            blurRadius: 15,
+            offset: Offset(15, 0),
+            spreadRadius: 5,
+          )
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(50),
+                border: Border.all(
+                    color: selected ? Colors.transparent : Colors.grey[200],
+                    width: 1.5)),
+            child: Icon(
+              categoryIcon,
+              color: Colors.black,
+              size: 30,
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            categoryName,
+            style: TextStyle(
+                fontWeight: FontWeight.w700, color: Colors.black, fontSize: 15),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(0, 6, 0, 10),
+            width: 1.5,
+            height: 15,
+            color: Colors.black26,
+          ),
+          Text(
+            availability.toString(),
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+              color: Colors.black,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+Widget searchBar() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: <Widget>[
+      Icon(
+        Icons.search,
+        color: Colors.black45,
+      ),
+      SizedBox(width: 20),
+      Expanded(
+        child: TextField(
+          decoration: InputDecoration(
+              hintText: "Search....",
+              contentPadding: EdgeInsets.symmetric(vertical: 10),
+              hintStyle: TextStyle(
+                color: Colors.black87,
+              ),
+              border: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.red))),
+        ),
+      ),
+    ],
+  );
+}
+
+Widget title() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.start,
+    children: <Widget>[
+      SizedBox(width: 45),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            "Food",
+            style: TextStyle(
+              fontWeight: FontWeight.w700,
+              fontSize: 30,
+            ),
+          ),
+          Text(
+            "Tukk",
+            style: TextStyle(
+              fontWeight: FontWeight.w200,
+              fontSize: 30,
+            ),
+          ),
+        ],
+      )
+    ],
+  );
+}
+
+  
